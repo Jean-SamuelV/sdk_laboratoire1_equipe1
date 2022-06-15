99e12e040a11e19be24181373c3bd7ac2e4cda79
Jean-SamuelV <92176378+Jean-SamuelV@users.noreply.github.com>
2022-06-15 19:18:59 -0400

    Premier Commit

---
+++ LibrairieConversionNote/ConversionAlphaToNote.cs
@@ -0,0 +1,29 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphaToNote
+    {
+        public string _note;
+
+        public ConversionAlphaToNote(string note)
+        {
+            _note = note;
+        }
+
+        public string transformer(string noteanglaise)
+        {
+            string input = noteanglaise;
+            List<string> noteenglish = new List<string>() { "A", "B", "C", "D", "E", "F", "G" };
+            List<string> notefrench = new List<string>() { "La", "Si", "Do", "Re", "Mi", "Fa", "Sol" };
+
+            for (int i = 0; i < noteenglish.Count; i++)
+            {
+                if (input == noteenglish[i])
+                {
+                    return notefrench[i];
+                }
+            }
+            return "Note not found! A to G only.";
+        }
+
+    }
+}
---
+++ SDK_Laboratoire1/Program.cs
@@ -0,0 +1,29 @@
+﻿using LibrairieConversionNote;
+using System;
+
+bool choixvalide = false;
+List<string> listenotes = new List<string>() { "A", "B", "C", "D", "E", "F", "G" };
+String reponse = "";
+while (choixvalide == false)
+{
+    Console.Write("Conversion a faire (options disponible: NOTE ou QUITTER): ");
+    reponse = Console.ReadLine().ToUpper();
+    if (reponse == "NOTE")
+    {
+        bool notecorrect = false;
+        while (notecorrect == false)
+        {
+            Console.Write("Conversion de Note: ");
+            string notetotranslate = Console.ReadLine().ToUpper();
+            ConversionAlphaToNote noteachanger = new ConversionAlphaToNote(notetotranslate);
+            string final = noteachanger.transformer(notetotranslate);
+            Console.WriteLine($"Note traduite: {final}");
+        }
+    }
+    else if (reponse == "QUITTER")
+    {
+        choixvalide = true;
+    }
+    choixvalide = false;
+}
+return 0;
